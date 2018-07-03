5245114ca56db3efb4629d884b92992e92cfb32c
Shakrah Yves <7shakrahs@gmail.com>
2018-07-03 16:41:15 +0200

    remove unused imports

--- src/main/scala/sy-node/TestServer.scala
+++ src/main/scala/sy-node/TestServer.scala
@@ -1,43 +1,28 @@
-import cats._
-import cats.{Id, MonadError, ~>, Monad}
-import cats.arrow.FunctionK
-import cats.evidence.As
-import cats.implicits._
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.language.{higherKinds, postfixOps}
+
 import co.upvest.arweave4s.adt.Data
 import co.upvest.arweave4s.adt.Transaction
 import co.upvest.arweave4s.adt.Wallet._
 import co.upvest.arweave4s.api
 import co.upvest.arweave4s.api.Config
-
-import io.circe
-import io.circe.parser.decode
-
 // Import response handler for Future
 import co.upvest.arweave4s.api.future._
-
-import scala.concurrent.ExecutionContext.Implicits.global
-import scala.language.{higherKinds, postfixOps}
-
 
 import com.softwaremill.sttp.asynchttpclient.future.AsyncHttpClientFutureBackend
 
 
 object TestTutorial extends App {
 
-  //then create a configuration context and weave-it-up.
-
-
   // Create configuration. Set the sttp's async backend for returning Futures
   val TestHost = (sys.env get "TESTNET_HOST" getOrElse "localhost") + ":1984"
   implicit val c = Config(host = TestHost, AsyncHttpClientFutureBackend())
-
-  val curBlock =  api.block.current()
 
   // Data to persist on the blockchain.
   val testData = Data(bytes = "Hi Mom!".getBytes("UTF-8"))
   // Let's get a new wallet
   val wallet = co.upvest.arweave4s.adt.Wallet.generate()
-
+  val curBlock =  api.block.current()
 
   for {
       // using the API method to estimate the price for the transaction in `Winstons`
