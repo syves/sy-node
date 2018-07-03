c031ab5cb4d0b0ee3babeb4b9030b78ab4587206
Shakrah Yves <7shakrahs@gmail.com>
2018-07-03 16:34:12 +0200

    resolved implicit issues with correct imports

--- src/main/scala/sy-node/TestServer.scala
+++ src/main/scala/sy-node/TestServer.scala
@@ -1,37 +1,37 @@
+import cats._
+import cats.{Id, MonadError, ~>, Monad}
+import cats.arrow.FunctionK
+import cats.evidence.As
+import cats.implicits._
 import co.upvest.arweave4s.adt.Data
 import co.upvest.arweave4s.adt.Transaction
 import co.upvest.arweave4s.adt.Wallet._
-
-import co.upvest.arweave4s.api //package includes id, price, ect, future, EncodedStringHandler
+import co.upvest.arweave4s.api
 import co.upvest.arweave4s.api.Config
 
+import io.circe
+import io.circe.parser.decode
+
 // Import response handler for Future
-import co.upvest.arweave4s.api.future
-import scala.concurrent.ExecutionContext.global
+import co.upvest.arweave4s.api.future._
 
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.{ExecutionContext, Future}
 import scala.language.{higherKinds, postfixOps}
-import scala.util.Try
 
-import com.softwaremill.sttp.{Response, SttpBackend, UriContext, sttp, asString}
-import com.softwaremill.sttp.HttpURLConnectionBackend
+
 import com.softwaremill.sttp.asynchttpclient.future.AsyncHttpClientFutureBackend
+
 
 object TestTutorial extends App {
 
   //then create a configuration context and weave-it-up.
-  //implicit val c = api.Config(host = "TestHost", HttpURLConnectionBackend())
 
-  /*def estimate[F[_], G[_]](d: Data)(implicit
-  c: AbstractConfig[F, G], esh: EncodedStringHandler[F]
-  */
 
   // Create configuration. Set the sttp's async backend for returning Futures
-  implicit val c = Config(host = "TestHost", AsyncHttpClientFutureBackend())
-  implicit val esh = EncodedStringHandler[String]
+  val TestHost = (sys.env get "TESTNET_HOST" getOrElse "localhost") + ":1984"
+  implicit val c = Config(host = TestHost, AsyncHttpClientFutureBackend())
 
-  //val curBlock =  api.block.current()
+  val curBlock =  api.block.current()
 
   // Data to persist on the blockchain.
   val testData = Data(bytes = "Hi Mom!".getBytes("UTF-8"))
@@ -39,10 +39,9 @@ object TestTutorial extends App {
   val wallet = co.upvest.arweave4s.adt.Wallet.generate()
 
 
-
   for {
       // using the API method to estimate the price for the transaction in `Winstons`
-      price    <- api.price.estimate(testData)(c, esh)
+      price    <- api.price.estimate(testData)
       // Get the last transaction of the sender wallet
       lastTx   <- api.address.lastTx(wallet)
       // Construct and send the transaction.
@@ -57,7 +56,7 @@ object TestTutorial extends App {
           )
         } yield ()
 
-  //println(wallet.owner)
-  //println(testData)
-  //println(curBlock)
+  println(wallet.owner)
+  println(testData)
+  println(curBlock)
 }
